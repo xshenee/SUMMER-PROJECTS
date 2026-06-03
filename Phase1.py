4cc10130a1b1bca92520bccd757112970e264af1
Celine <celine.he.23@ucl.ac.uk>
2026-06-03 22:11:50 +0100

    Initial commit

---
+++ Phase1.py
@@ -0,0 +1,12 @@
+total = 0
+count = 0
+
+for i in range(1,101):
+    total = total + i
+    count = count + 1
+
+average = total / count
+
+print("The total is", total)
+print("The average is", average)
+print("The calculated average is:", average)
