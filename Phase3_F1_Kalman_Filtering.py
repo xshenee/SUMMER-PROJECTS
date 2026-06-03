076de1ae0e23a3f8ecfcb43629ac5ae9a0bff1a4
Celine <celine.he.23@ucl.ac.uk>
2026-06-03 22:42:27 +0100

    Phase 3

--- Phase3_F1_Kalman_Filtering.py
+++ Phase3_F1_Kalman_Filtering.py
@@ -13,14 +13,42 @@ session.load()
 hamilton = session.laps.pick_driver('HAM')
 verstappen = session.laps.pick_driver('VER')
 
-raw_lap_rimes = hamilton['LapTime'].dt.total_seconds()
-raw_lap_rimes_verstappen = verstappen['LapTime'].dt.total_seconds()
+raw_lap_times = hamilton['LapTime'].dt.total_seconds()
+raw_lap_times_verstappen = verstappen['LapTime'].dt.total_seconds()
 
-clean_lap_times = raw_lap_rimes.dropna()
-clean_lap_times_verstappen = raw_lap_rimes_verstappen.dropna()
+clean_lap_times = raw_lap_times.dropna()
+clean_lap_times_verstappen = raw_lap_times_verstappen.dropna()
 
 print("Hamilton's Clean Lap Times (seconds):")
 print(clean_lap_times)
 print("\nVerstappen's Clean Lap Times (seconds):")
 print(clean_lap_times_verstappen)
-print(clean_lap_times.head(5))
+
+import numpy as np
+
+lap_times = clean_lap_times.values
+n_laps = len(lap_times)
+
+x_est = np.zeros(n_laps)
+P_est = np.zeros(n_laps)
+
+x_est[0] = lap_times[0]
+P_est[0] = 1.0
+
+Q = 0.05
+R = 4.0
+
+for i in range(1, n_laps):
+    x_pred = x_est[i-1]
+    P_pred = P_est[i-1] + Q
+
+    measurement = lap_times[i]
+
+    K = P_pred / (P_pred + R)
+
+    x_est[i] = x_pred + K * (measurement - x_pred)
+
+    P_est[i] = (1 - K) * P_pred
+
+print("Raw Lap", round(lap_times[9], 2), "seconds")
+print("Filtered Lap", round(x_est[9], 2), "seconds")
